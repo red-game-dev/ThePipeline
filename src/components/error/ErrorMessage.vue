4afa98a9574282f452c90b653e64db8a3b9b9ef6
Red <red.pace.dev@gmail.com>
2023-09-16 13:57:44 +0200

    feat: Added error message

---
+++ src/components/error/ErrorMessage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts" generic="T extends Error, O extends Error">
+interface Props {
+  error?: Error | null
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  error: null,
+})
+</script>
+
+<template>
+  <div border-red bg-red p-3 text-white>
+    <span v-if="isError(props.error)">
+      {{ props.error?.message || props.error }}
+    </span>
+    <span v-else>
+      An unexpected error occurred.
+    </span>
+  </div>
+</template>
