5b0ba98ae8d5a4a17f1a3ef662ddcb86ce03fdec
Red <red.pace.dev@gmail.com>
2023-09-16 14:06:45 +0200

    feat: Added user store

---
+++ src/store/user.ts
@@ -0,0 +1,40 @@
+import type { UserProfile } from '~/@types'
+import { useErrorsStore } from '~/store'
+
+interface State {
+  profile?: UserProfile | null
+}
+
+export const useUserStore = defineStore('user', {
+  state: (): State => ({
+    profile: null,
+  }),
+  getters: {
+    getProfile: (state: State) => state.profile,
+  },
+  actions: {
+    async fetchProfile() {
+      const errorStore = useErrorsStore()
+
+      const { abort, canAbort, error, data } = await profileRequest()
+
+      if (error.value) {
+        errorStore.add({ errors: [error.value], key: 'userProfile' })
+
+        return
+      }
+
+      const { password: _password = null, ...profile } = data?.value || {}
+
+      this.$patch({
+        profile,
+      })
+
+      return {
+        abort,
+        canAbort,
+        data,
+      }
+    },
+  },
+})
