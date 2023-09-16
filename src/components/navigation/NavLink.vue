bb2e3ec72f69a2bd89a03c12f5500508d85b6d56
Red <red.pace.dev@gmail.com>
2023-09-16 13:56:31 +0200

    feat: Added navigation bar

---
+++ src/components/navigation/NavLink.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+import type { RouterLinkProps } from 'vue-router'
+
+const props = defineProps<RouterLinkProps>()
+</script>
+
+<template>
+  <router-link :to="props.to" v-bind="$attrs" text-md p-4 font-bold no-underline hover:text-teal-500>
+    <slot />
+  </router-link>
+</template>
---
+++ src/components/navigation/Navigation.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+import { useAuthStore } from '~/store'
+
+const authState = useAuthStore()
+
+const NAVIGATION_ITEMS = ref({
+  logged: {
+    '/dashboard/overview': 'Overview',
+  },
+  guest: {
+    '/': 'Home',
+  },
+  authLinks: {
+    '/auth/signin': 'Login',
+  },
+})
+
+const navigationLinks = computed(() => authState.isAuthenticated
+  ? {
+      ...NAVIGATION_ITEMS.value.logged,
+      ...NAVIGATION_ITEMS.value.guest,
+    }
+  : {
+      ...NAVIGATION_ITEMS.value.guest,
+      ...NAVIGATION_ITEMS.value.authLinks,
+    })
+</script>
+
+<template>
+  <header class="mx-auto flex justify-between px-4 py-5 lg:py-7 lg:py-8">
+    <Logo />
+    <div class="flex flex-row">
+      <NavLink
+        v-for="([route, label], index) in Object.entries(navigationLinks)"
+        :key="`navigation-${index}`"
+        :to="route"
+      >
+        {{ label }}
+      </NavLink>
+    </div>
+  </header>
+</template>
