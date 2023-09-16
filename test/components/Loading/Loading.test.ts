9f575f10f2a60a4820005578bf9df22dd95008f9
Red <red.pace.dev@gmail.com>
2023-09-16 20:03:45 +0200

    fix: Fixed test breaking for file path

--- test/components/Loading/Loading.test.ts
+++ test/components/Loading/Loading.test.ts
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
-import Loading from '../../../src/components/Loading/Loading.vue'
+import Loading from '../../../src/components/loading/Loading.vue'
 
 describe('Forms/Loading.vue', () => {
   it('should have loading visible', async () => {
