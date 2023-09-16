75525f1fb996d536d90bdc69bb20ceecfe14d3e9
Red <red.pace.dev@gmail.com>
2023-09-16 13:59:49 +0200

    feat: Added text input

---
+++ src/components/Forms/fields/TextInput.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const modelValue = defineModel()
+</script>
+
+<template>
+  <input
+    v-model="modelValue"
+    type="text"
+    v-bind="$attrs"
+    w-full rounded-md bg-teal-500 px-3 py-2 text-white placeholder:text-gray-300
+  >
+</template>
