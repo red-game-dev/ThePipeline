957449054b5d68e0fed9d5b999a6f9aabcbefc87
Red <red.pace.dev@gmail.com>
2023-09-16 14:01:01 +0200

    feat: Added user details to be used for overview

---
+++ src/components/user/UserDetails.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+interface Props {
+  name: string
+  email: string
+  avatar: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div w-64 rounded-lg p-8 shadow-teal-500>
+    <div flex flex-col items-center>
+      <img mb-4 h-24 w-24 rounded-full :src="avatar" alt="User Profile Image">
+      <h2 mb-1 text-2xl font-semibold>
+        {{ props.name }}
+      </h2>
+      <p text-sm text-teal-500>
+        {{ props.email }}
+      </p>
+    </div>
+  </div>
+</template>
